116b742b45df3f3e157d1a3bc6fa8dac7b6cdf76
leeminjin0827 <lmj98202@gmail.com>
2025-04-14 15:50:08 +0900

    04-14 / 메인 화면, 페이지 위젯 기능

---
+++ lib/app/main.dart
@@ -0,0 +1,8 @@
+// * main.dart : 플러터 실행 하는 파일
+import 'package:flutter/cupertino.dart';
+import 'package:tj2024b_app/app/layout/myapp.dart';
+
+void main(){
+  // runApp( 최초실행할위젯() ); 최초 위젯을 실행 하는 함수
+  runApp( MyApp() ); // MaterialApp 갖는 위젯
+}
